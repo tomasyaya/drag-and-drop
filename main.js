6413f3777a8da8b08cd6c195284cd793187aff10
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro-2.local>
2020-06-27 10:09:10 +0200

    dragn and drop

---
+++ main.js
@@ -0,0 +1,5 @@
+const right = document.querySelector('#right');
+const left = document.querySelector('#left');
+const containers = [left, right]
+const options = {copy: true}
+dragula(containers, options);
